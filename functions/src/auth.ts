fe71afe60035eb0aeb4655a1b9f100e250a5e250
agishevatatiana <tatiana.agisheva@hiqo-solutions.com>
2020-07-04 15:50:08 +0300

    first commit

---
+++ functions/src/auth.ts
@@ -0,0 +1,14 @@
+import { ServiceAccount } from "firebase-admin";
+
+export const auth = <ServiceAccount>{
+    type: "service_account",
+    project_id: "jira-react-53875",
+    private_key_id: "92fb7837cb9efe60b2924bec6576cb2a11db7f2f",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCsJLUORA+8CcQR\nw2cSinlHeRM43bLmFms1Yd2GqtqDBs8HBxTeohaK1oe2eI/fITJGqDttFKS92SvL\nda1HDDnRSbfQ4B940fj+cMDzjvzf7ACBCZmb0RZ4A0qBTvoRNOscxKRqYHybhfUA\nviSwyTPj35YlKk93F+k1dhFdz1mJvzSLkCcO3MaiqVdouRh8VlPss3dgNc3Gu8Oh\nJBcEVti+i6XxEtAdD5fPABPs6CupROTTzpwB5D/nY5lBpr4gnkBWDbJUmfegJPjI\nRykPLROWhqta2U9yEr9Dgq9Ie4fo+YMckphFO7RNxXNrodIBfkpd5vYA8R6bIACp\nju1g2/q/AgMBAAECggEAAY3Dor91OZD+2fe/5jccFpLSsZMrS5mDpc3edkmCUUsS\n+gGTtXrNPbGukw58TYNMsZhQXBzST3mhNesWrosGUXZyyMsj5nh7Tc+Xp3GnMo0b\nF2RoZs/rrVALCFshOkp+oqEd5ftjJ4FStqLpBcJdO3H/gkr/h2YyPZW3u8f6D/k7\nWcYmCrjXEp9uf1TJvB/aL8EwAyCqbRghV8Vu90M4s6DxA9C4zlyar7SZbBrYOjuW\nFFXIAYuEA8S8txdcc0YrlMiSF/iNyPlOjn4RUihYApXOeGSsJVkXkDOHXV7AuvL1\n55fKgLODJL8IRHVdoAP3mIISmUIFDeM3x/XOGs7GmQKBgQDm+DFaho2whZw3g5ZI\n37HkU8BJJ8U4sKf9t2nUX+HU09QPMEarzI6JLZs6tpmClKoTfaF2xse3y+TyCJfN\nlEpsq7iC8Cj6EzOCuSQwb5NcATnclSJZO+kGLdAs0ehK/w7I+UWIVoI9H5/MahbL\nmqLTEGUDeihpRm29uEO5TZ0N5wKBgQC+zH8SbMJCm9fdpcOIYPp4iQIHGDm7w0gJ\nkGauV3Bf3YksE3+Z3yo71Up9h1QJ2fb7p2vWdi7sMeXYFHz9Xlp+Qt/3JWznkkn9\nXOKqTn8wfkY0N/4ag/FXQ4f7OdkilGDK55cSnuNFZVp8DbL/AgekAlMvmsCfgg0y\n2E7amFWhaQKBgQC6+eUKQa7mXeNO1YmNyPk7u8kH79gqwmfcbuSPdmddYXZkLVKo\ntVoWFwN5DmVLXoooaSWS/+yiD79xaB9r30Y4nvMpnL2vZGfoxXRUeZ6zdpKezWQX\nFPbZd7D096CzRbN0Ysc1TcL1NGqqkp+Nh8uUTctYe0XHo6onUcoRcpFXtQKBgBRT\nqP4HhDKgc8DzVf+DhWut/P9Qf2YXiecq8SWFvz0XVvPRJ/j2o4M4V/VDo5phFET/\nMyfn81vKfRgmJqK/mJh2+3FTFWUeWq+ZY7SNJFPAQC50AKhGgkGvuh7zrMRYVm+H\neLrNHp1YDOnGc9ljF/lLHh2mOPtFWa6fCNeU/xpxAoGAHy4Xzl1Os0d7KaozxHUM\n26ZqmopfQhvEvy+ZrJoqY7AqENEf1mhvuPAs0RUMUkrAUDRjIfOsJwS2frvZvh82\nQlq0DFA93xVa6L8YITxYpdJ+meIUzBn4zIySliqrt0VukfdTlm+AANgOHjY+grPj\nJVslUzagS9xwtMbUCqbdg1A=\n-----END PRIVATE KEY-----\n",
+    client_email: "firebase-adminsdk-qw0uk@jira-react-53875.iam.gserviceaccount.com",
+    client_id: "109662184923601869184",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qw0uk%40jira-react-53875.iam.gserviceaccount.com"
+}
---
+++ functions/src/models.ts
@@ -0,0 +1,18 @@
+export interface Project {
+    lead: string;
+    name: string
+}
+
+export interface Task {
+    project_key: string;
+    reporter: string;
+    description: string;
+    status: string;
+    summary: string;
+}
+
+export interface User {
+    userId: string;
+    email: string;
+    full_name: string;
+}
