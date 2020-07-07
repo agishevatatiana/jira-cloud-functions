b0a11842e12ad765fa40380db06b0f6e401fc229
agishevatatiana <tatiana.agisheva@hiqo-solutions.com>
2020-07-07 13:43:02 +0300

    add authorization checking

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,11 +23,29 @@ import { getTasks, createTask } from "./tasks";
 import { getUsers } from "./users";
 import { signUp, logIn } from "./authentication";
 
-app.get('/projects', getProjects);
-app.post('/project', createProject);
-app.get('/tasks', getTasks);
-app.post('/task', createTask);
-app.get('/users', getUsers);
+const FBAuth = (async (req: any, res: any, next: any) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        return res.status(403).json({ error: 'Unauthorized'});
+    }
+
+    try {
+        const decToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decToken;
+        return next();
+    } catch (e) {
+        console.error('Error while verifying token ', e);
+        return res.json(403).json(e);
+    }
+});
+
+app.get('/projects', FBAuth, getProjects);
+app.post('/project', FBAuth, createProject);
+app.get('/tasks', FBAuth, getTasks);
+app.post('/task', FBAuth, createTask);
+app.get('/users', FBAuth, getUsers);
 app.post('/signup', signUp);
 app.post('/login', logIn);
 
