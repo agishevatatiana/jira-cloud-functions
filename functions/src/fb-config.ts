015b72c40da2f8d335b79943e6767589149d2487
Tatiana Agisheva <41381975+agishevatatiana@users.noreply.github.com>
2021-01-26 19:21:09 +0300

    Update fb-config.ts

--- functions/src/fb-config.ts
+++ functions/src/fb-config.ts
@@ -1,10 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB_ZCaUvNQ-XnflKdzxMSS4HNuowR7rf4o",
-    authDomain: "jira-react-53875.firebaseapp.com",
-    databaseURL: "https://jira-react-53875.firebaseio.com",
-    projectId: "jira-react-53875",
-    storageBucket: "jira-react-53875.appspot.com",
-    messagingSenderId: "274036535540",
-    appId: "1:274036535540:web:b8bd7e1a60a471f2382ea1",
-    measurementId: "G-H8NQQRFPED"
+
 };
